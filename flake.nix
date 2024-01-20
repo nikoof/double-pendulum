87ae9b60de3b9372eba37ea10360426efaeea0f0
Nicolas Bratoveanu <nicolasbratoveanu@proton.me>
2024-01-20 22:50:03 +0200

    feat: switch to crane and add gh-pages workflow

--- flake.nix
+++ flake.nix
@@ -3,29 +3,22 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
 
-    naersk.url = "github:nix-community/naersk/master";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, utils, naersk, fenix, ... }:
+  outputs = { self, nixpkgs, utils, crane, fenix, ... }:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        toolchain = with fenix.packages.${system};
-          combine [
-            stable.rustc
-            stable.cargo
-            stable.rustfmt
-            stable.rust-analyzer
-            targets.wasm32-unknown-unknown.stable.rust-std
-          ];
-        naerskLib = pkgs.callPackage naersk {
-          rustc = toolchain;
-          cargo = toolchain;
-        };
+
         libPath = with pkgs;
           lib.makeLibraryPath [
             libGL
@@ -36,18 +29,59 @@
             xorg.libXrandr
             xorg.libXi
           ];
-      in
-      {
-        defaultPackage = naerskLib.buildPackage {
-          pname = "double-pendulum";
-          src = ./.;
+
+        toolchain = with fenix.packages.${system};
+          combine [
+            stable.rustc
+            stable.cargo
+            stable.rustfmt
+            stable.rust-analyzer
+            targets.wasm32-unknown-unknown.stable.rust-std
+          ];
+
+        craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+
+          src = pkgs.lib.cleanSourceWith {
+            src = ./.;
+            filter = path: type:
+              (pkgs.lib.hasSuffix "\.html" path) ||
+              (pkgs.lib.hasSuffix "\.scss" path) ||
+              (pkgs.lib.hasInfix "/assets/" path) ||
+              (craneLib.filterCargoSources path type);
+          };
+
+          commonArgs = {
+            inherit src;
+            strictDeps = true;
+          };
+
+          cargoArtifacts = craneLib.buildDepsOnly {
+            inherit src;
+            doCheck = false;
+          };
+      in rec {
+        packages.default = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
 
           LD_LIBRARY_PATH = libPath;
+        });
+
+        packages.gh-pages = craneLib.buildTrunkPackage (commonArgs // {
+          inherit (pkgs) wasm-bindgen-cli;
+          inherit cargoArtifacts;
+
+          CARGO_BUILD_TARGET = "wasm32-unknown-unknown";
+        });
+
+        apps.default = utils.lib.mkApp {
+          drv = packages.default;
         };
 
-        devShell = with pkgs; mkShell {
-          buildInputs = [ toolchain trunk ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        devShells.default = craneLib.devShell {
+          packages = with pkgs; [
+            trunk
+          ];
+
           LD_LIBRARY_PATH = libPath;
         };
       });
