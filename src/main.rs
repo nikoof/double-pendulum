68662c71ef4df466a8d1c65ee0e7b838842f4821
Nicolas Bratoveanu <nicolasbratoveanu@proton.me>
2024-01-20 02:13:54 +0200

    fix: window name

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod pendulum;
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result<()> {
     eframe::run_native(
-        "demo",
+        "Double Pendulum Demo",
         eframe::NativeOptions::default(),
         Box::new(|_cc| Box::new(app::App::default())),
     )
